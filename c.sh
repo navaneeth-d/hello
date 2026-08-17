ec51bf12aff25c474dc2f975ba5db411ae0caed8
Navaneeth D <144593800+navaneeth-d@users.noreply.github.com>
2026-08-17 09:06:29 +0530

    Create c.sh

---
+++ c.sh
@@ -0,0 +1,24 @@
+# !/bin/zsh
+# A simple calculator shell program
+a = $1
+b = $2
+
+echo "Enter Choice:"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Muliply"
+echo "4. Division"
+
+ch = $3
+
+case $ch in 
+    1) res = `expr $a + $b`
+    ;;
+    2) res = `expr $a - $b`
+    ;;
+    3) res = `expr $a * $b`
+    ;;
+    4) res = `expr $a / $b`
+    ;;
+esac
+echo "Result: $res"
