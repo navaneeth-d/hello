7cbeade02707fb16e2d6de2bc4eb5536085dc670
Navaneeth D <Navaneeth.D@iiitb.ac.in>
2026-09-02 09:19:27 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,7 +9,7 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         data = [1, 2, 3, 4, 5]
         result = summation(data)
-        self.assertEqual(result, 5)
+        self.assertEqual(result, 15)
 
 if __name__ == '__main__':
     unittest.main()
